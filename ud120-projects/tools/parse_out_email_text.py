ceafc73d933f0632c7b0df8172e03c1b5088dfab
klausdorf <Frie1dolin!>
2018-05-27 14:21:24 +0200

    completed clustering and text analysis

--- ud120-projects/tools/parse_out_email_text.py
+++ ud120-projects/tools/parse_out_email_text.py
@@ -28,15 +28,15 @@ def parseOutText(f):
         text_string = content[1].translate(str.maketrans("", "", string.punctuation))
 
         ### project part 2: comment out the line below
-        words = text_string
+        ### words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+        splitcontent = text_string.split()
+        stemmer = SnowballStemmer("english")
+        for word in splitcontent:
+            words = words+' '+stemmer.stem(word)
 
     return words
 
